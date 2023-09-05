faf6ad5df0302f7e9b32e0d19e1f7cf0738fe404
Murilo Ferreira <115593380+Murilo57@users.noreply.github.com>
2023-09-05 00:14:17 -0300

    Mascara de formatação %
    Mascara de formatação como: %s: string, %d: inteiro, %2.f: float com 2 casas decimais e %r: boolean

---
+++ ExibirVariavel.py
@@ -0,0 +1,9 @@
+codigo = 105
+nome = 'Manuel Da Silva'
+salario = 16500.00
+ativo = True
+
+print("Código: %d"% codigo)
+print("Nome: %s"% nome)
+print("Salário: %.2f " % salario)
+print("Ativo: %r "% ativo)
